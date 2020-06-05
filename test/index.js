2e391499bd3243a3d71818940cd56c9f27abfc91
shihao6 <shihao6@guazi.com>
2020-06-05 16:59:23 +0800

    commit: delete test

--- test/index.js
+++ test/index.js
@@ -1,11 +1,3 @@
-const path = require('path');
 
-const fs = require('fs');
 
-var obj = {
-  a: 1,
-  b: 2,
-  c: '123'
-};
-
-fs.writeFileSync(path.join(__dirname, 'dist/test/test.json'), JSON.stringify(obj));
+console.log('test cases');
